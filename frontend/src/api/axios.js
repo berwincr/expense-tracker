dc553470aceff4247dc3974839c88bdd45d0e69f
berwincr <berwincr4028@gmail.com>
2026-07-10 08:20:36 +0530

    Restore axios interceptor

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -28,6 +28,7 @@ api.interceptors.response.use(
 
 export default api;
 */
+/*
 import axios from "axios";
 
 console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
@@ -35,5 +36,44 @@ console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
+
+export default api;*/
+
+import axios from "axios";
+
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  console.log("TOKEN:", token);
+  console.log("REQUEST:", config.url);
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+
+      if (!window.location.pathname.startsWith("/login")) {
+        window.location.href = "/login";
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
